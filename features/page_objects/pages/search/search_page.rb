3bebb8f3ab307de61db8701ca4f05a0edb79307a
Jairo QA <jairoam85@gmail.com>
2023-03-23 17:47:34 -0300

    refatorando

--- features/page_objects/pages/search/search_page.rb
+++ features/page_objects/pages/search/search_page.rb
@@ -2,7 +2,6 @@ require_relative '../../sections/products/products_list'
 module Pages
     class SearchPage < SitePrism::Page
         set_url ''
-
         sections :products, Sections::ProductsList, '.ajax_block_product'
     end
 end
